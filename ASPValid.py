3ba1c8f9b613fb50f3747df20c4bfb7b8ccd5d41
Ethan Fenichel <EthanFenichel@gmail.com>
2016-07-07 15:35:51 -0400

    updated to check each URL

--- ASPValid.py
+++ ASPValid.py
@@ -2,19 +2,20 @@
 from pymarc import *
 import csv
 
-marcFile = 'c://users/fenichele//desktop//test.mrc'
+marcFile = 'faavonallb.mrc'
 
 def testURL(url):
     import requests
 
     result = 0
-    testphrase = 'You do not have access to this page'
+    testphraseList = ['You do not have access to this page', 'HTTP Error 500 - Internal Server Error']
 
     r = requests.get(url)
     rtext = r.text
 
-    if testphrase in rtext:
-        result = 1
+    for phrase in testphraseList:
+        if phrase in rtext:
+            result = 1
 
     return result
 
@@ -24,7 +25,7 @@ def logResults(recordID, title, url, urlresult):
     now = time.strftime('%Y-%m-%d %H:%M:%S')
     data = [[now, str(recordID), str(title), str(url), str(urlresult)]]
 
-    resultsFile = 'c:\\users\\fenichele\\desktop\\asp.csv'
+    resultsFile = 'accessChecks.csv'
 
     with open(resultsFile, 'a', newline='') as out:
         a = csv.writer(out, delimiter=',', quoting=csv.QUOTE_ALL)
@@ -35,26 +36,51 @@ def logResults(recordID, title, url, urlresult):
             a.writerows(data)
 
 def readMarc():
-
     with open(marcFile,'rb') as m:
         reader = MARCReader(m)
+        counter = 0
+
 
         for record in reader:
+            urlList = []
+            counter += 1
+
+            startRecord = 4860
+            if counter < startRecord:
+               continue
+            else:
+                print('starting with record '+str(startRecord))
+			
             # return record
             if record['856'] is None:
-                url = None
+                urls = None
             else:
-                url = record['856']['u']
+                urls = record.get_fields('856')
+                # print(url)
             recordID = record['001'].value()
             title = record['245']['a']
             # print (url)
-            if url is None:
+            if urls is None:
+                url = None
                 urlresult = 1
             else:
-                urlresult = testURL(url)
+                for x in urls:
+                    if x['u'] is None:
+                        url = None
+                    elif x['u'].replace('http://ezproxy.fau.edu/login?url=','') not in urlList:
+                        urlList.append(x['u'].replace('http://ezproxy.fau.edu/login?url=',''))
+                    else:
+                        url = None
+
+                for url in urlList:
+                    urlresult = testURL(url)
+                    logResults(recordID, title, url, urlresult)
 
 
-            logResults(recordID, title, url, urlresult)
+            continueTest = 'y'
+            # continueTest = str(input('continue? press "n" to stop'))
+            if continueTest == 'n':
+                print(1/0)
 
 
 readMarc()
